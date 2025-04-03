74c7dd0ef00dca28eb5ecd3a79e7da3df81a3b97
Toomas-A <58360070+Toomas-A@users.noreply.github.com>
2025-04-03 23:09:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://toomas-a.github.io"],  # Лучше конкретно, а не "*"
+    allow_origins=["https://toomas-a.github.io"],  # ❗ Только этот адрес
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
